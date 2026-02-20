236654645031d35bbeb63bb306212277b8d154cf
Vishalaggarwal270 <vishalaggarwal@192.168.1.7>
2026-02-20 15:22:48 +0530

    push

--- app/about/github-contributions/calendar.tsx
+++ app/about/github-contributions/calendar.tsx
@@ -1,12 +1,43 @@
 'use client';
 
-import { ThemeInput } from 'react-activity-calendar';
+import ActivityCalendar, { Activity, ThemeInput } from 'react-activity-calendar';
 
 export const GITHUB_THEME: ThemeInput = {
   light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
   dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'],
 };
 
-export default function Calendar() {
-  return <section className="flex flex-col space-y-4"></section>;
+interface CalendarProps {
+  data: Activity[];
+}
+
+export default function Calendar({ data }: CalendarProps) {
+  return (
+    <section className="flex flex-col space-y-4">
+      <ActivityCalendar
+        data={data}
+        theme={GITHUB_THEME}
+        hideTotalCount
+        colorScheme="dark"
+        labels={{
+          months: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+          ],
+          weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+          totalCount: '{{count}} contributions in the last year',
+        }}
+      />
+    </section>
+  );
 }
